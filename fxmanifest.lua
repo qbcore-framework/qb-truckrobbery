a0f2e386a61962e59c951b26c0f73e283e026764
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-23 00:28:11 +0300

    rework truck robbery

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,9 @@ game 'gta5'
 description 'QB-TruckRobbery'
 version '1.0.0'
 
-shared_script '@qb-core/import.lua'
-server_script 'server/main.lua'
+shared_scripts { 
+	'config.lua'
+}
+
 client_script 'client/main.lua'
+server_script 'server/main.lua'
