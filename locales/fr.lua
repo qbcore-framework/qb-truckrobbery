15cc7dfe788d316d32901be17322fcbab690784c
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-05-27 05:22:51 +0200

    Refactor
    *Added Qb-Target compatibility
    *Added Locales
    *Updated event names to english
    *fixed a bug where the explosion would send the truck flying in a direction
    *fixed police alerts
    *Added Config File

---
+++ locales/fr.lua
@@ -0,0 +1,42 @@
+local Translations = {
+    error = {
+     get_out_water = 'Sortez de l\'eau',  
+     guards_dead = 'Les gardes doivent être morts pour placer la bombe',
+     truck_ismoving = 'Vous ne pouvez pas braquer un camion en mouvement.',
+     activepolice = 'Il faut au moins %{ActivePolice} Policiers pour activer la mission.',
+     alreadyactive = 'Quelqu\'un est déjà en train de faire cette mission',
+    },
+    success = {
+      packing_cash = 'Vous êtes entrain de remplir le sac!',
+      took_bags = 'Vous avez pris %{bags} sacs de cash du camion',
+    },
+    mission = {
+      activation_cost = "Vous avez besoin de $ %{ActivationCost} en banque pour accepter la mission",
+      accept_mission_target = 'Accepter la mission',
+      accept_mission = '~g~[E]~b~ Accepter la mission',
+      stockade = 'Stockade',
+      sender = "Le Boss",
+      subject ="Nouvelle cible",
+      message = "Alors vous-voulez vous faire de l'argent? bon... allez trouvez une arme et faites le boulot.. Je vous envoie la localisation.",
+    },
+    info = {
+      before_bomb = 'Débarassez vous des gardes avant de placer la bombe.',
+      detonate_bomb = 'Appuyez sur [G] pour exploser la porte arrière et prendre le cash',
+      detonate_bomb_target = 'Posez la bombe',
+      plant_bomb = 'Poser la bombe',
+      bomb_timer = 'La bombe explosera dans %{TimeToBlow} Secondes',
+      collect = 'Vous pouvez commencer à collecter l\'argent.',
+      take_money_target = 'Prendre l\'argent',
+      take_money = 'Appuyez sur [E] pour prendre l\'argent',
+      cop_Blip = "10-90: Braquage de Transport de Fonds",
+      alerttitle = "Braquage de Transport de Fonds",
+      alert_desc = "Un Transport de Fonds se fait braquer!",
+      policeblip = 'Assaut sur le Transport de fonds',
+      grabing_money = 'Vous prenez l\'argent..'
+    },      
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
