b8a7580d0afc7a04e3c8a5f120a81a7289931611
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-01-15 00:34:37 -0600

    Money reasons

--- server/main.lua
+++ server/main.lua
@@ -5,25 +5,25 @@ RegisterServerEvent('AttackTransport:akceptujto', function()
 	local copsOnDuty = 0
 	local _source = source
 	local xPlayer = QBCore.Functions.GetPlayer(_source)
-	local accountMoney = xPlayer.PlayerData.money["bank"]
+	local accountMoney = xPlayer.PlayerData.money['bank']
 	if ActiveMission == 0 then
 		if accountMoney < Config.ActivationCost then
-			TriggerClientEvent('QBCore:Notify', _source, "You need " .. Config.Currency .. "" ..Config.ActivationCost.. " in the bank to accept the mission")
+			TriggerClientEvent('QBCore:Notify', _source, 'You need ' .. Config.Currency .. '' .. Config.ActivationCost .. ' in the bank to accept the mission')
 		else
 			for _, v in pairs(QBCore.Functions.GetPlayers()) do
 				local Player = QBCore.Functions.GetPlayer(v)
 				if Player ~= nil then
-					if (Player.PlayerData.job.name == "police" or Player.PlayerData.job.type == "leo") and Player.PlayerData.job.onduty then
+					if (Player.PlayerData.job.name == 'police' or Player.PlayerData.job.type == 'leo') and Player.PlayerData.job.onduty then
 						copsOnDuty = copsOnDuty + 1
 					end
 				end
 			end
 			if copsOnDuty >= Config.ActivePolice then
-				TriggerClientEvent("AttackTransport:Pozwolwykonac", _source)
-				xPlayer.Functions.RemoveMoney('bank', Config.ActivationCost, "armored-truck")
+				TriggerClientEvent('AttackTransport:Pozwolwykonac', _source)
+				xPlayer.Functions.RemoveMoney('bank', Config.ActivationCost, 'armored-truck')
 				OdpalTimer()
 			else
-				TriggerClientEvent('QBCore:Notify', _source, 'Need at least '..Config.ActivePolice.. ' police to activate the mission.')
+				TriggerClientEvent('QBCore:Notify', _source, 'Need at least ' .. Config.ActivePolice .. ' police to activate the mission.')
 			end
 		end
 	else
@@ -32,39 +32,39 @@ RegisterServerEvent('AttackTransport:akceptujto', function()
 end)
 
 RegisterServerEvent('qb-armoredtruckheist:server:callCops', function(streetLabel, coords)
-    -- local place = "Armored Truck"
-    -- local msg = "The Alarm has been activated from a "..place.. " at " ..streetLabel
+	-- local place = "Armored Truck"
+	-- local msg = "The Alarm has been activated from a "..place.. " at " ..streetLabel
 	-- Why is this unused?
-    TriggerClientEvent("qb-armoredtruckheist:client:robberyCall", -1, streetLabel, coords)
+	TriggerClientEvent('qb-armoredtruckheist:client:robberyCall', -1, streetLabel, coords)
 end)
 
 function OdpalTimer()
 	ActiveMission = 1
-	Wait(Config.ResetTimer*1000)
+	Wait(Config.ResetTimer * 1000)
 	ActiveMission = 0
 	TriggerClientEvent('AttackTransport:CleanUp', -1)
 end
 
-RegisterServerEvent('AttackTransport:zawiadompsy', function(x ,y, z)
-    TriggerClientEvent('AttackTransport:InfoForLspd', -1, x, y, z)
+RegisterServerEvent('AttackTransport:zawiadompsy', function(x, y, z)
+	TriggerClientEvent('AttackTransport:InfoForLspd', -1, x, y, z)
 end)
 
 RegisterServerEvent('AttackTransport:graczZrobilnapad', function()
 	local _source = source
 	local xPlayer = QBCore.Functions.GetPlayer(_source)
-	local bags = math.random(1,3)
+	local bags = math.random(1, 3)
 	local info = {
 		worth = math.random(Config.Payout.Min, Config.Payout.Max)
 	}
 	xPlayer.Functions.AddItem('markedbills', bags, false, info)
-	TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['markedbills'], "add")
+	TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['markedbills'], 'add')
 
 	local chance = math.random(1, 100)
-	TriggerClientEvent('QBCore:Notify', _source, 'You took '..bags..' bags of cash from the van')
+	TriggerClientEvent('QBCore:Notify', _source, 'You took ' .. bags .. ' bags of cash from the van')
 
 	if chance >= 95 then
 		xPlayer.Functions.AddItem('security_card_01', 1)
-		TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['security_card_01'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['security_card_01'], 'add')
 	end
 	Wait(2500)
 end)
