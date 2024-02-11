6969a740a365e37c863580d3369d9cfd9f77aac3
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-02-11 06:00:54 -0600

    Description for txAdmin

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'QB-TruckRobbery'
+lua54 'yes'
+author 'Kakarot'
+description 'Allows players to rob specific trucks for money and items'
 version '1.2.0'
 
 shared_script 'config.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
-
-lua54 'yes'
